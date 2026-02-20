e7c5b446cf8dca68eec71d1e305767042df46901
SherbanevIvan <ivan.sherbanev228@mail.ru>
2026-02-20 09:23:16 +0300

    Пофиксил/улучшил логику работы скрипта

--- script.sh
+++ script.sh
@@ -3,16 +3,22 @@
 
 A=()
 AB=()
-
-if [[ $(( $2 % 2 )) == 0 ]]; then
-	A+=$2
-else
-	AR+=$2
-fi
-
 number=$1
 N=()
 NA=()
+
+if [ $# -lt 2 ]; then
+
+	echo Мало чисел
+	exit 1
+
+elif [ $# -gt 2 ]; then
+
+	echo Многа чисел
+	exit 1
+
+fi
+
 
 if [ $((number % 2)) == 0 ]; then 
 
@@ -22,4 +28,14 @@ else
 
 	NA+=$number
 	echo Список NA: ${NA[@]}
+fi
+
+if [[ $(( $2 % 2 )) == 0 ]]; then
+
+        A+=$2
+        echo Список A: ${A[@]}
+
+else
+        AR+=$2
+        echo Список AB: ${AR[@]}
 fi
